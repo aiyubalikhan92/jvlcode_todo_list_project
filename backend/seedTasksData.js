59a1b027b16f81dae240fc169cdfc2edd05d8843
aiyubalikhan92 <aiyubalikhan92@gmail.com>
2025-12-06 08:28:03 +0000

    Update MongoDB and API URLs with EC2 public IP

--- backend/seedTasksData.js
+++ backend/seedTasksData.js
@@ -1,7 +1,7 @@
 // server/seedTasksData.js
 const mongoose = require('mongoose');
 
-const MONGO_URL = process.env.MONGO_URI || 'mongodb://34.204.14.4:27017/todo';
+const MONGO_URL = process.env.MONGO_URI || 'mongodb://54.242.183.194:27017/todo';
 
 // Define minimal schema
 const taskSchema = new mongoose.Schema({
